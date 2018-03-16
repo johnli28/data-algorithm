6e4ed5a1d430ac35fb5a5ba6c78034d57511e141
zhuang.li <zhuang.li@thomsonreuters.com>
2018-03-16 11:44:11 +0800

    Refine the previous recursive implementation of lcs problem to dynamic programming

--- dynamic-program/lcs.h
+++ dynamic-program/lcs.h
@@ -4,21 +4,42 @@
 #include <string>
 #include "utils.h"
 
+#include <vector>
+
 using namespace std;
 
-int lcs(char* lStr, int lLen, char* rStr, int rLen)
+// NOTE:
+// dynamic programming: split a big problem into small problems
+// A dynamic programming table, store the states (results) of small problems
+// Initiate states
+
+int lcsDP(char* lStr, int lLen, char* rStr, int rLen, string & lcsStr)
 {
+	vector<vector<int>> table;
+
 	int lcsLen = 0;
-	if (lLen == 0 || rLen == 0)
+
+	for (int i = 0; i <= lLen; i++)
 	{
-		return lcsLen;
-	}
-	else if (lStr[lLen - 1] == rStr[rLen - 1])
-	{
-		lcsLen = 1 + lcs(lStr, lLen - 1, rStr, rLen - 1);
-	}
-	else
-	{
-		lcsLen = max(lcs(lStr, lLen - 1, rStr, rLen), lcs(lStr, lLen, rStr, rLen - 1));
-	}
+		table.push_back(vector<int>(rLen + 1));
+
+		for (int j = 0; j <= rLen; j++)
+		{
+			if (i == 0 || j == 0)
+			{
+				table[i][j] = 0;
+			}
+			else if (lStr[i - 1] == rStr[j - 1])
+			{
+				table[i][j] = 1 + table[i - 1][j - 1];
+				lcsStr.append(1, lStr[i - 1]);
+			}
+			else
+			{
+				table[i][j] = max(table[i - 1][j], table[i][j - 1]);
+			}
+		}
+	}	
+
+	return table[lLen][rLen];
 }
--- dynamic-program/main.cpp
+++ dynamic-program/main.cpp
@@ -5,5 +5,11 @@ void main()
 	char a[] = "a1btyu2dop";
 	char b[] = "rqb82gmnd";
 
-	int ret = lcs(a, sizeof(a) - 1, b, sizeof(b) - 1);
+	//int ret = lcs(a, sizeof(a) - 1, b, sizeof(b) - 1);
+
+
+
+	string lcsStr;
+
+	int ret2 = lcsDP(a, sizeof(a) - 1, b, sizeof(b) - 1, lcsStr);
 }
